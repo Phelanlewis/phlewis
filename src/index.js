3957f75f0708d9a07cd23e7fc2d5de33f7b9dc0b
Phelan Lewis <phelanlewis@gmail.com>
2017-05-30 22:20:49 -0700

    added index.js with AppInitializer func

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App.js';
+import Home from './components/Home.js';
+
+import {
+  browserHistory,
+  IndexRoute,
+  Redirect,
+  Route,
+  Router
+} from  'react-router';
+
+class AppInitializer {
+  run() {
+    render(
+      <Router history={browserHistory}>
+        <Route path="/" component={ App }>
+          <IndexRoute component={ Home } />
+        </Route>
+        <Redirect from="*" to="/" />
+      </Router>
+    )
+  }
+}
+
+new AppInitializer().run();
